e16f573b36d7b18d40e039420fda6cc0cfcbf322
Roberto Costa <roberto.araujo339@gmail.com>
2023-05-27 14:09:11 -0300

    getting airport code from arguments

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import sys
+
+code = str(sys.argv[1])
+
+print(f'Cod. informado: {code}')
 
 def news():
-    url='https://aisweb.decea.mil.br/?i=aerodromos&codigo=SBMT'
+    url=f'https://aisweb.decea.mil.br/?i=aerodromos&codigo={code}'
       
     resp=requests.get(url)
 
@@ -13,18 +18,27 @@ def news():
         
         cartas = []
 
-        sunrise = soup.find('sunrise').text
-        print(sunrise)
-        sunset = soup.find('sunset').text
-        print(sunset)
+       
         
         elements = soup.find_all('ul',class_='list list-icons list-primary list-icons-style-2')
-
+        
+        cartas = []
         for element in elements:
             sibling = element.find_previous_sibling('h4')
             cartas.append(sibling.text)
+        
+        print("Cartas:",cartas)
+        sunrise = soup.find('sunrise').text
+        print("Nascer do Sol: ", sunrise)
+        sunset = soup.find('sunset').text
+        print("Pôr do Sol: ", sunset)
+    
+        tafMetarElements = soup.find_all('h5',class_='mb-0 heading-primary')
 
-        print(cartas)
+        for el in tafMetarElements:
+            sibling = el.find_next_sibling('p').text
+            print(el.text + " " + sibling)
+
       
     else:
         print("Error")
